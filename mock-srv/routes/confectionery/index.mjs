225f0fc5e6fa069e34e2b11252cd049466d36c12
simoganger <simonsoft7@gmail.com>
2024-03-09 20:56:16 -0500

    Creating a real-time app in NodeJs using fastify

---
+++ mock-srv/routes/confectionery/index.mjs
@@ -0,0 +1,20 @@
+'use strict';
+const data = [
+    {
+        id: "B1",
+        name: "Chocolate Bar",
+        rrp: "22.40",
+        info: "Delicious overprices chocolate."
+    }
+]
+
+export default async function (fastify, opts) {
+    fastify.get("/", async function (request, reply) {
+        return data;
+    });
+    fastify.post("/", async function (request, reply) {
+        fastify.mockDataInsert(request, opts.prefix.slice(1), data);
+        return data;
+    })
+}
+
